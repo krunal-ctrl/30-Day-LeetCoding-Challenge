839b38b739c82e559dc694ced0f5aee5c0ae7d1f
krunal-ctrl <50491905+krunal-ctrl@users.noreply.github.com>
2020-04-30 09:31:24 +0530

    Create Middle_of_the_Linked_List.java

---
+++ April/Middle_of_the_Linked_List.java
@@ -0,0 +1,36 @@
+/*
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+
+Input: [1,2,3,4,5]
+Output: Node 3 from this list (Serialization: [3,4,5])
+The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+Note that we returned a ListNode object ans, such that:
+ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+*/
+ 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int i=1;
+        ListNode temp=head;
+        while(head.next!=null){
+                head=head.next;
+                i++;
+        }
+        i/=2;
+        for(int j=0;j<i;j++)
+            temp=temp.next;
+        return (temp);
+    }
+}
